c0c44bd0e65cec652fe03e6776d222c63153fe29
nihalshanp <pnihalshan94@gmail.com>
2021-08-09 07:19:24 +0530

    Learned Yup validation schema

--- src/Components/YoutubeForm.js
+++ src/Components/YoutubeForm.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const YoutubeForm = () => {
   const initialValues = {
@@ -12,34 +13,39 @@ const YoutubeForm = () => {
     console.log("Form values", values);
   };
 
-  const validate = (values) => {
-    // values.name values.emial values.channel
-    // errors.name errors.emial errors.channel
+  // const validate = (values) => {
+  //   // values.name values.emial values.channel
+  //   // errors.name errors.emial errors.channel
 
-    let errors = {};
+  //   let errors = {};
 
-    if (!values.name) {
-      errors.name = "Required";
-    }
-    if (!values.email) {
-      errors.email = "Required";
-    } else if (
-      !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
-    ) {
-      errors.email = "Invalid email address";
-    }
+  //   if (!values.name) {
+  //     errors.name = "Required";
+  //   }
+  //   if (!values.email) {
+  //     errors.email = "Required";
+  //   } else if (
+  //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+  //   ) {
+  //     errors.email = "Invalid email address";
+  //   }
 
-    if (!values.channel) {
-      errors.channel = "Required";
-    }
+  //   if (!values.channel) {
+  //     errors.channel = "Required";
+  //   }
 
-    return errors;
-  };
+  //   return errors;
+  // };
 
+  const validationSchema = Yup.object({
+    name: Yup.string().required("Required!"),
+    email: Yup.string().email("Invalid email format").required("Required"),
+    channel: Yup.string().required("Required"),
+  });
   const formik = useFormik({
     initialValues,
     onSubmit,
-    validate,
+    validationSchema,
   });
 
   console.log("Form touched", formik.touched);
